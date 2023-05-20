99a8dcabe7e02717925c1f4b98d345f292ad333e
atabekdemurtaza <mychannel21war@gmail.com>
2023-05-20 15:49:56 +0500

    Configure an app

--- blog/urls.py
+++ blog/urls.py
@@ -5,7 +5,8 @@ app_name = 'blog'
 
 urlpatterns = [
     # представление поста
-    path(route='', view=views.post_list, name='post_list'),
+    #path(route='', view=views.post_list, name='post_list'), # ПП
+    path(route='', view=views.PostListView.as_view(), name='post_list'), # ООП
     path(route='<int:year>/<int:month>/<int:day>/<slug:post>/', view=views.post_detail, name='post_detail'),
 ]
 
--- blog/views.py
+++ blog/views.py
@@ -4,6 +4,17 @@ from django.shortcuts import get_object_or_404
 from django.core.paginator import Paginator
 from django.core.paginator import EmptyPage
 from django.core.paginator import PageNotAnInteger
+from django.views.generic import ListView
+from .forms import EmailPostForm
+
+class PostListView(ListView):
+    """
+        Альтернативное представление списка постов
+    """
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 2
+    template_name = 'blog/post/list.html'
 
 def post_list(request):
     post_list = Post.published.all()
@@ -39,3 +50,27 @@ def post_detail(request,year, month, day, post):
     }
     return render(request=request, template_name='blog/post/detail.html', context=context)
 
+
+def post_share(request, post_id):
+    # Изввлечь пост по идентификатору id
+    post = get_object_or_404(
+        klass=Post,
+        id=post_id,
+        status=Post.Status.PUBLISHED
+    )
+
+    if request.method == 'POST':
+        # Форма была передана на обработку
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            # Поля формы успешно прошли валидацию
+            cd = form.cleaned_data
+            # ... отправить электронное письмо
+    else:
+        form = EmailPostForm()
+    context = {
+        'post': post,
+        'form': form
+    }
+
+    return render(request=request, template_name='blog/post/share.html', context=context)
